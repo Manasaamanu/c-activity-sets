b8e757f67891ff1690a65f74317c8cd19dee2f25
Manasaamanu <Manasac1.bsc23@rvu.edu.in>
2024-01-29 20:57:14 +0530

    test

--- problem03.c
+++ problem03.c
@@ -1,31 +1,30 @@
-
 #include <stdio.h>
 int input();
-int add(int a,int b );
+int add(int a,int b);
 void output(int a,int b,int sum);
 int main()
 {
-    int a, b,sum;
-    a=input();
-    b=input();
-    sum=add(a,b);
-    output(a,b,sum);
-    return 0;
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum=add(a,b);
+  output(a,b,sum);
+  return 0;
 }
 int input()
 {
-    int x;
-    printf("enter the values\n");
-    scanf("%d",&x);
-    return x;
+  int x;
+  printf("Enter the number:");
+  scanf("%d,&n");
+  return 0;
 }
 int add(int a,int b)
 {
-    int sum;
-    sum=a+b;
-    return sum;
+  int sum;
+  sum=a+b;
+  return sum;
 }
 void output(int a,int b,int sum)
 {
-    printf("The sum of %d,%d is %d\n",a,b,sum);
+  printf("The sum of %d, %d is %d/n,a,b,sum");
 }
--- problem04.c
+++ problem04.c
@@ -1,30 +1,27 @@
-
 #include <stdio.h>
 int input();
-void add(int a,int b ,int *sum);
-void output(int a,int b,int sum);
+void add(int a,int b,int *sum);
+void output(?int a,int b,int sum);
 int main()
 {
-    int a, b,sum;
-    a=input();
-    b=input();
-    add(a,b,& sum);
-    output(a,b,sum);
+ int a,b,sum;
+ a=input();
+ b=input();
+ add(a,b,sum);
+ output(a,b,sum);
 }
 int input()
 {
-    int x;
-    printf("enter the values\n");
-    scanf("%d",&x);
-    return x;
+  int number;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return x;
 }
-void add(int a,int b,int*sum)
+void add(int a,intb,int*sum)
 {
-   
-    *sum=a+b;
-    
+  *sum=a+b;
 }
 void output(int a,int b,int sum)
 {
-    printf("The sum of %d,%d is %d\n",a,b,sum);
+  printf("The sum of %d.%d is %d\n"a,b,sum);
 }
