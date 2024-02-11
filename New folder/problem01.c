1839b0d792d7c985b4da70832645f134b866f0c3
Manasaamanu <Manasac1.bsc23@rvu.edu.in>
2024-02-11 21:19:01 +0530

    test

--- New folder/problem01.c
+++
@@ -1,8 +0,0 @@
-// 1. Write a C program to print your name.
-#include <stdio.h>
-
-int main()
-{
-  printf("manasa");
-  return 0;
-}
--- problem05.c
+++ problem05.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 int input();
 int compare(int a,int b,int c,int largest );
