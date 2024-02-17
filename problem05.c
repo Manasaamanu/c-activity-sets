22b13112234797b4c1f3791a9ff081603fd740a7
Manasaamanu <Manasac1.bsc23@rvu.edu.in>
2024-02-17 21:48:21 +0530

    manasa

--- problem05.c
+++ problem05.c
@@ -1,42 +1,38 @@
 #include <stdio.h>
 int input();
-int compare(int a,int b,int c,int largest );
+int compare(int a,int b,int c,int largest);
 void output(int a,int b,int c,int largest);
-int main()
-{
-    int a, b,c,largest;
-    a=input();
-    b=input();
-    c=input();
-    largest=compare(a,b,c,largest);
-    output(a,b,c,largest);
-    return 0;
+int main(){
+  int a,b,c,largest;
+  a=input();
+  b=input();
+  c=input();
+  largest = compare(a,b,c,largest);
+  output(a,b,c,largest);
+  return 0;
 }
-int input()
-{
-    int x;
-    printf("enter the values\n");
-    scanf("%d",&x);
-    return x;
+int input(){
+  int number;
+  printf("Enter the number\n");
+  scanf("%d",&number);
+  return 0;
 }
-int compare(int a,int b,int c,int largest )
-{
-  if(a>=b&&a>=c)
+int compare(int a,int b,int c,int largest){
+  if (a>=b && a>=c)
   {
     largest=a;
     return a;
-  }
-  if(b>=a&&b>=c)
+  } 
+  if (b>=a && b>=c)
   {
     largest=b;
-    return a;
+    return b;
   }
   else{
     largest=c;
     return c;
   }
 }
-void output(int a,int b,int c,int largest)
-{
-    printf("the largest of %d,%d,%d is %d\n",a,b,c,largest);
+void output(int a,int b,int c,int largest){
+  printf("The largest of %d,%d,%d is %d\n",a,b,c,largest);
 }
