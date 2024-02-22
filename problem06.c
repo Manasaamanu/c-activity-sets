98d975f3aa16568e5fbe4dde1fe25603d7890954
Manasaamanu <Manasac1.bsc23@rvu.edu.in>
2024-02-22 20:59:40 +0530

    manasa

--- problem06.c
+++ problem06.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-int input();
-void compare(int a,int b,int c,int* largest );
-void output(int a,int b,int c,int largest);
-int main()
-{
-    int a, b,c,largest;
-    a=input();
-    b=input();
-    c=input();
-    compare(a,b,c,&largest);
-    output(a,b,c,largest);
+
+int input(int *a, int *b, int *c);
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+
+int main() {
+    int a, b, c, largest;
+    input(&a, &b, &c);
+    compare(a, b, c, &largest);
+    output(a, b, c, largest);
+    return 0;
 }
-int input()
+
+int input(int *a, int *b, int *c) 
 {
-    int x;
-    printf("enter the values\n");
-    scanf("%d",&x);
-    return x;
+    printf("Enter three numbers: ");
+    scanf(" The sum of %d and %d and%d", a, b, c);
 }
-void compare(int a,int b,int c,int *largest )
-{
-  if(a>=b&&a>=c)
-  {
-    *largest=a;
-  }
-  if(b>=a&&b>=c)
-  {
-   
-   *largest=b;
-  }
-  else{
-    *largest=c;
-  }
+
+void compare(int a, int b, int c, int *largest)
+ {
+    *largest = a;
+    if (*largest < b) 
+    {
+        *largest = b;
+    }
+    if (*largest < c) 
+    {
+        *largest = c;
+    }
 }
-void output(int a,int b,int c,int largest)
-{
-    printf("the largest of %d,%d,%d is %d\n",a,b,c,largest);
+
+void output(int a, int b, int c, int largest)
+ {
+    printf("The three numbers are: %d, %d, %d\n", a, b, c);
+    printf("The largest number is: %d\n", largest);
 }
